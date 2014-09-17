5b4566120a42db848e91d2194b3be16681b8ad37
Jürgen Viljaste <j.viljaste@gmail.com>
2014-09-18 00:12:28 +0300

    Update

---
+++ svn-export.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+WORKING_DIR="$(pwd)"
+
+if [ "${1}" == "-h" ] || [ "${1}" == "--help" ]; then
+  cat << EOF
+    svn-export 31337:HEAD archive
+EOF
+
+  exit 1
+fi
+
+case "${1}" in
+  -r)
+    mailcatcher "${@:2}"
+  ;;
+  *)
+    output_error "svn-export: Unknown command. See 'svn-export --help'"
+
+    exit 1
+  ;;
+esac
