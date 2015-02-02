0611cf2c405d753595735b4dd1774954d415f934
Jürgen Viljaste <j.viljaste@gmail.com>
2015-02-02 21:53:40 +0200

    Update

--- svn-export.sh
+++ svn-export.sh
@@ -64,7 +64,7 @@ set "${ARGUMENTS[@]}"
 OPTIONS=""
 
 if [ -n "${USERNAME}" ] && [ -n "${PASSWORD}" ]; then
-  OPTIONS="--username ${USERNAME} --password ${PASSWORD}"
+  OPTIONS="--no-auth-cache --username ${USERNAME} --password ${PASSWORD}"
 fi
 
 if [ "${#}" -lt 2 ] || [ "${#}" -gt 3 ]; then
